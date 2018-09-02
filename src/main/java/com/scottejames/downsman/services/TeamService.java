324a57f7e475a00504f3e379cacaea2e81a363bb
Scott James <scott.james@r3.com>
2018-09-02 18:36:38 +0100

    Corrected some logic in team validation

--- src/main/java/com/scottejames/downsman/services/TeamService.java
+++ src/main/java/com/scottejames/downsman/services/TeamService.java
@@ -146,7 +146,7 @@ public class TeamService {
                             results.add("For B-Class team size must be 4 your team is " + teamSize);
                         if (leader == true)
                             results.add("For B-Class leaders may not hike");
-                        if (combinedAge > 48)
+                        if (combinedAge < 48)
                             results.add("For B-Class combined age must be more than 48 your combined age is " + combinedAge);
                         if (serviceCrew == false)
                             results.add("Service crew required for B-Class");
@@ -158,7 +158,7 @@ public class TeamService {
                             results.add("For A-Class team size must be 3 your team is " + teamSize);
                         if (leader == true)
                             results.add("For A-Class leaders may not hike");
-                        if (combinedAge > 48)
+                        if (combinedAge < 48)
                             results.add("For A-Class combined age must be more than 48 your combined age is " + combinedAge);
                         if (serviceCrew == false)
                             results.add("Service crew required for A-Class");
@@ -167,7 +167,7 @@ public class TeamService {
                             results.add("For V-Class team size must be 3 your team size is " + teamSize);
                         if (leader == true)
                             results.add("For V-Class leaders may not hike");
-                        if (combinedAge > 100)
+                        if (combinedAge < 100)
                             results.add("For V-Class combined age must be more than 100 your combined age is " + combinedAge);
                         if (serviceCrew == false)
                             results.add("Service crew required for V-Class");
@@ -177,7 +177,7 @@ public class TeamService {
                             results.add("For S-Class team size must be 4 your team size is " + teamSize);
                         if (leader == true)
                             results.add("For S-Class leaders may not hike");
-                        if (combinedAge > 48)
+                        if (combinedAge < 48)
                             results.add("For S-Class combined age must be more than 48 your combined age is " + combinedAge);
                         if (maxAge < 14.5)
                             results.add("For S-Class max age must be less than 14.5 your max age is " + maxAge);
@@ -189,7 +189,7 @@ public class TeamService {
                             results.add("For E-Class team size must be 4 your team size is " + teamSize);
                         if (leader == true)
                             results.add("For E-Class leaders may not hike");
-                        if (combinedAge > 48)
+                        if (combinedAge < 48)
                             results.add("For E-Class combined age must be more than 48 your combined age is " + combinedAge);
                         if (combinedAge < 62)
                             results.add("For E-Class combined age must be less than 62 your combined age is " + combinedAge);
