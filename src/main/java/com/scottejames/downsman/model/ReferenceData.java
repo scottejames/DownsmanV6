0f8ae4f4011600d3a1cdac4d156756a09866a036
Scott James <scott.james@r3.com>
2018-09-17 12:40:30 +0100

    Updated bank details - going offline for VCS until this can be externalised.

--- src/main/java/com/scottejames/downsman/model/ReferenceData.java
+++ src/main/java/com/scottejames/downsman/model/ReferenceData.java
@@ -7,5 +7,5 @@ public class ReferenceData {
     public static final String [] START_TIMES = {"06:00","06:15","06:30","06:45","07:00","07:15","07:30","07:45","08:00","08:15","08:30","08:45","09:00","09:15","09:30"};
     public static final LocalDate HIKE_DATE = LocalDate.of(2018,10,6);
 
-    public static final String BANK_DETAILS = "REFERENCE: XXXXX";
+    public static final String BANK_DETAILS = "A/C: 00028235 S/C: 40-52-40";
 }
