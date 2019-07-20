b108aa40fc111e1da942b8d991a3dd68e9e3938c
Scott James <scottejames@gmail.com>
2019-07-20 13:58:35 +0100

    Updated to use https!!

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,4 @@
-export DM_VERSION=2.1
+export DM_VERSION=2.2
 export DM_WAR="../target/downsman-1.13-SNAPSHOT.war"
 export DM_CLASSES="../target/downsman-1.13-SNAPSHOT/WEB-INF/classes/"
 export AWS_REGION="eu-west-2"
